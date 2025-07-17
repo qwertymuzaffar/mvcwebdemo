75f2f8bb68d5eb14c44cc62e5093995aa56b21dc
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-07-16 20:18:01 -0400

    Connecting MySQL with Java

--- src/main/java/com/example/mvcwebdemo/MvcwebdemoApplication.java
+++ src/main/java/com/example/mvcwebdemo/MvcwebdemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.mvcwebdemo;
 
+import java.sql.*;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,20 @@ public class MvcwebdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MvcwebdemoApplication.class, args);
+
+        String url = "jdbc:mysql://localhost:3306/MY_DATABASE"; // Change to your database
+        String user = "root"; // Your database username
+        String password = "admin"; // Your database password
+
+        try {
+            // Establishing connection
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement statement = conn.createStatement();
+            System.out.println("connection successful!");
+            // Your JDBC code here
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
