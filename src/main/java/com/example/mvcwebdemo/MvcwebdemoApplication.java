0b5178344ebc513c29f548c86e4e0f4b57ed3428
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-07-17 21:12:40 -0400

    Prepared and Callable Statements

--- src/main/java/com/example/mvcwebdemo/MvcwebdemoApplication.java
+++ src/main/java/com/example/mvcwebdemo/MvcwebdemoApplication.java
@@ -17,14 +17,48 @@ public class MvcwebdemoApplication {
         String password = "admin"; // Your database password
 
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            // Establishing connection
-            Statement statement = conn.createStatement();
-            System.out.println("connection successful!");
-
-
+            System.out.println("Connection successful!");
+            insertStudent(conn);
+            updateStudent(conn);
+            getStudentsByMajor(conn);
+            callGetStudentsByMajor(conn);
+            callGetAverageGPA(conn);
+            insertStudentWithGeneratedKey(conn);
+            simulateSQLError(conn);
+            System.out.println("------------------------");
+            System.out.println("Finish!");
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void insertStudent(Connection conn) throws SQLException {
+        System.out.println("------------------------");
+        System.out.println("Insert data into the Students table");
+    }
+    public static void updateStudent(Connection conn) throws SQLException {
+        System.out.println("------------------------");
+        System.out.println("Update Students with GPA of 3.9 and name of Alice");
+    }
+    public static void getStudentsByMajor(Connection conn) throws SQLException {
+        System.out.println("------------------------");
+        System.out.println("Get all students with major of Computer Science");
+    }
+    public static void callGetStudentsByMajor(Connection conn) throws SQLException {
+        System.out.println("------------------------");
+        System.out.println("Calling stored procedure GetStudentsByMajor");
+    }
+    public static void callGetAverageGPA(Connection conn) throws SQLException {
+        System.out.println("------------------------");
+        System.out.println("Calling stored procedure GetAverageGPA");
+    }
+    public static void insertStudentWithGeneratedKey(Connection conn) throws SQLException {
+        System.out.println("------------------------");
+        System.out.println("Insert student and retrieve generated key");
+    }
+    public static void simulateSQLError(Connection conn) {
+        System.out.println("------------------------");
+        System.out.println("Simulating SQL Error");
     }
 
 }
