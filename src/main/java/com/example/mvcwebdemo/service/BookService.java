d5851f9934cdd70aba616e35cdde6c154a6b5f48
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-07-22 20:34:15 -0400

    Fetching All Books

--- src/main/java/com/example/mvcwebdemo/service/BookService.java
+++ src/main/java/com/example/mvcwebdemo/service/BookService.java
@@ -18,4 +18,8 @@ public class BookService {
     public Book saveBook(Book book) {
         return bookRepository.save(book);
     }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
 }
