b466db78e7b2180876f7805c4bd2dd5c29dfddea
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-07-22 20:36:56 -0400

    Creating a Controller

---
+++ src/main/java/com/example/mvcwebdemo/controller/BookController.java
@@ -0,0 +1,52 @@
+package com.example.mvcwebdemo.controller;
+
+import com.example.mvcwebdemo.entity.Book;
+import com.example.mvcwebdemo.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+    // Create a new book
+
+    @PostMapping("/addBook")
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book savedBook = bookService.saveBook(book);
+        return ResponseEntity.ok(savedBook);
+    }
+
+    // Get all books
+    @GetMapping("/getBooks")
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    // Get a book by ID
+    @GetMapping("/getBook/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Optional<Book> book = bookService.getBookById(id);
+        return book.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    // Update a book
+    @PutMapping("/updateBook/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book bookDetails) {
+        Book updatedBook = bookService.updateBook(id, bookDetails);
+        return ResponseEntity.ok(updatedBook);
+    }
+
+    // Delete a book
+    @DeleteMapping("/deleteBook/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+        return ResponseEntity.noContent().build();
+    }
+}
