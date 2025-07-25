c2f6978b31f42fc1b2ac096990c43328430b9240
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-07-25 17:57:58 -0400

    Creating the Book Entity

--- src/main/java/com/example/mvcwebdemo/entity/Book.java
+++ src/main/java/com/example/mvcwebdemo/entity/Book.java
@@ -1,55 +1,65 @@
 package com.example.mvcwebdemo.entity;
 
+
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
 
 @Entity
 public class Book {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotNull
-    @Size(min = 2, max = 100)
     private String title;
-
-    @NotNull
     private String author;
-
-    @Min(1)
     private double price;
+    private String genre;
 
-    // Getters and setters
+    // Getter for id
     public Long getId() {
         return id;
     }
 
+    // Setter for id
     public void setId(Long id) {
         this.id = id;
     }
 
+    // Getter for title
     public String getTitle() {
         return title;
     }
 
+    // Setter for title
     public void setTitle(String title) {
         this.title = title;
     }
 
+    // Getter for author
     public String getAuthor() {
         return author;
     }
 
+    // Setter for author
     public void setAuthor(String author) {
         this.author = author;
     }
 
+    // Getter for price
     public double getPrice() {
         return price;
     }
 
+    // Setter for price
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    // Getter for genre
+    public String getGenre() {
+        return genre;
+    }
+
+    // Setter for genre
+    public void setGenre(String genre) {
+        this.genre = genre;
     }
 }
